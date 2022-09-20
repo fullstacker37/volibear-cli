a1ec01452fcfbdd466c8c7b86f80cd2a55f838af
volibear <zhaop2014@sina.com>
2022-09-20 09:40:54 +0800

    feat: 使用commander处理自定义指令

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,15 @@
 #! /usr/bin/env node
 
-console.log(234)
+const { program } = require('commander')
+
+program.option('-f --framework <framework>', '设置框架')
+program
+  .command('create <project> [option]') // 注册命令
+  .alias('crt') // 别名
+  .description('创建项目') // 命令描述
+  .action((project, args) => { // 命令行执行逻辑代码
+    
+    console.log(project, args)
+  })
+
+program.parse(process.argv)
