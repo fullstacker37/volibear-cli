11b1dffca2d6ece099d8ed86b74ef1dc0f86591f
volibear <zhaop2014@sina.com>
2022-09-20 22:24:47 +0800

    feat: 逻辑代码模块化拆分

--- bin/cli.js
+++ bin/cli.js
@@ -1,15 +1,11 @@
 #! /usr/bin/env node
 
 const { program } = require('commander')
-
-program.option('-f --framework <framework>', '设置框架')
-program
-  .command('create <project> [option]') // 注册命令
-  .alias('crt') // 别名
-  .description('创建项目') // 命令描述
-  .action((project, args) => { // 命令行执行逻辑代码
-    
-    console.log(project, args)
-  })
+// 添加help选项
+const help = require('../lib/core/help')
+help(program)
+// 注册命令
+const registerCommanders = require('../lib/core/commander')
+registerCommanders(program)
 
 program.parse(process.argv)
